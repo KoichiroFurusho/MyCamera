7720979ca76e7d56ceec6677913935b1e77f12f9
KoichiroFurusho <kouichiro.jp@gmail.com>
2021-03-02 00:21:56 +0900

    CameraApp

--- MyCamera/ViewController.swift
+++ MyCamera/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UINavigationControllerDelegate , UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var pictureImage: UIImageView!
+ 
+    
+    @IBAction func cameraButtonAction(_ sender: Any) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            print("カメラは利用できます。")
+            let imagePickerCntroller = UIImagePickerController()
+            
+            imagePickerCntroller.sourceType = .camera
+            
+            imagePickerCntroller.delegate = self
+            
+            present(imagePickerCntroller, animated: true, completion: nil)
+        } else {
+            print("カメラは利用できません。")
+        }
+    }
+    
+    
+    @IBAction func shareButtonAction(_ sender: Any) {
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        
+        pictureImage.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
